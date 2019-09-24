13c3c0ed743f1f6231ba3902488204a58fe59a97
Aleix Penella <aleix.penella@edreamsodigeo.com>
2019-09-24 08:25:20 +0200

    myexecutor-ansibleplaybook

---
+++ examples/myexecutor-ansibleplaybook/myexecutor-ansibleplaybook.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	ansibler "github.com/apenella/go-ansible"
+)
+
+type MyExecutor struct{}
+
+func (e *MyExecutor) Execute(command string, args []string, prefix string) error {
+	fmt.Println("I am doing nothing")
+
+	return nil
+}
+
+func main() {
+
+	ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{
+		Connection: "local",
+	}
+
+	ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
+		Inventory: "127.0.0.1,",
+	}
+
+	playbook := &ansibler.AnsiblePlaybookCmd{
+		Playbook:          "site.yml",
+		ConnectionOptions: ansiblePlaybookConnectionOptions,
+		Options:           ansiblePlaybookOptions,
+		Exec:              &MyExecutor{},
+	}
+
+	err := playbook.Run()
+	if err != nil {
+		panic(err)
+	}
+}
