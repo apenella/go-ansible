9cc6e70084bdb659e6d089c74a1ec9f94c6d762f
Aleix Penella <aleix.penella@edreamsodigeo.com>
2021-12-10 18:53:29 +0100

    include extra-vars file example

---
+++ examples/extravars-file-ansibleplaybook/extravars-file-ansibleplaybook.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	"github.com/apenella/go-ansible/pkg/options"
+	"github.com/apenella/go-ansible/pkg/playbook"
+)
+
+func main() {
+
+	ansiblePlaybookConnectionOptions := &options.AnsibleConnectionOptions{
+		Connection: "local",
+		User:       "aleix",
+	}
+
+	ansiblePlaybookOptions := &playbook.AnsiblePlaybookOptions{
+		Inventory: "127.0.0.1,",
+		ExtraVarsFile: []string{
+			"@vars-file1.yml",
+			//"@vars-file2.yml",
+		},
+	}
+
+	playbook := &playbook.AnsiblePlaybookCmd{
+		Playbooks:         []string{"site.yml"},
+		ConnectionOptions: ansiblePlaybookConnectionOptions,
+		Options:           ansiblePlaybookOptions,
+	}
+
+	err := playbook.Run(context.TODO())
+	if err != nil {
+		panic(err)
+	}
+}
