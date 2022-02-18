eab10a685d2387e27f723d423158309b392d9fc8
Aleix Penella <aleix.penella@edreamsodigeo.com>
2022-02-18 15:15:32 +0100

    new example with measurements

--- examples/ansibleplaybook-time-measurement/ansibleplaybook-time-measurement.go
+++ examples/ansibleplaybook-time-measurement/ansibleplaybook-time-measurement.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 
+	"github.com/apenella/go-ansible/pkg/execute"
+	"github.com/apenella/go-ansible/pkg/execute/measure"
 	"github.com/apenella/go-ansible/pkg/options"
 	"github.com/apenella/go-ansible/pkg/playbook"
 )
@@ -17,10 +19,18 @@ func main() {
 		Inventory: "127.0.0.1,",
 	}
 
+	executorTimeMeasurement := measure.NewExecutorTimeMeasurement(
+		execute.NewDefaultExecute(
+			execute.WithEnvVar("ANSIBLE_FORCE_COLOR", "true"),
+		),
+		measure.WithShowDuration(),
+	)
+
 	playbook := &playbook.AnsiblePlaybookCmd{
-		Playbooks:         []string{"site.yml", "site2.yml"},
+		Playbooks:         []string{"site.yml"},
 		ConnectionOptions: ansiblePlaybookConnectionOptions,
 		Options:           ansiblePlaybookOptions,
+		Exec:              executorTimeMeasurement,
 	}
 
 	err := playbook.Run(context.TODO())
