02b2be44267f2da9eb25a737886f74fc864adce3
Aleix Penella <aleix.penella@gmail.com>
2024-02-19 22:06:39 +0100

    Define the executor AnsiblePlaybookExecute for basic purposes

--- examples/ansibleplaybook-simple/ansibleplaybook-simple.go
+++ examples/ansibleplaybook-simple/ansibleplaybook-simple.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"os"
 
-	"github.com/apenella/go-ansible/pkg/execute"
 	"github.com/apenella/go-ansible/pkg/options"
 	"github.com/apenella/go-ansible/pkg/playbook"
 )
@@ -19,20 +19,13 @@ func main() {
 		Inventory: "127.0.0.1,",
 	}
 
-	playbook := &playbook.AnsiblePlaybookCmd{
-		Playbooks:         []string{"site.yml", "site2.yml"},
-		ConnectionOptions: ansiblePlaybookConnectionOptions,
-		Options:           ansiblePlaybookOptions,
-	}
+	err := playbook.NewAnsiblePlaybookExecute("site.yml", "site2.yml").
+		WithOptions(ansiblePlaybookOptions).
+		WithConnectionOptions(ansiblePlaybookConnectionOptions).
+		Execute(context.TODO())
 
-	log.Println("Command: ", playbook)
-
-	exec := execute.NewDefaultExecute(
-		execute.WithCmd(playbook),
-	)
-
-	err := exec.Execute(context.TODO())
 	if err != nil {
-		panic(err)
+		fmt.Print()
+		os.Exit(1)
 	}
 }
--- pkg/playbook/ansiblePlaybookBuilder.go
+++ pkg/playbook/ansiblePlaybookBuilder.go
@@ -1,57 +1,57 @@
 package playbook
 
-import (
-	"github.com/apenella/go-ansible/pkg/options"
-)
+// import (
+// 	"github.com/apenella/go-ansible/pkg/options"
+// )
 
-// AnsiblePlaybookCmdBuilder is a builder for the AnsiblePlaybookCmd struct
-type AnsiblePlaybookCmdBuilder struct {
-	cmd *AnsiblePlaybookCmd
-}
+// // AnsiblePlaybookCmdBuilder is a builder for the AnsiblePlaybookCmd struct
+// type AnsiblePlaybookCmdBuilder struct {
+// 	cmd *AnsiblePlaybookCmd
+// }
 
-// NewAnsiblePlaybookCmdBuilder return a new AnsiblePlaybookCmdBuilder
-func NewAnsiblePlaybookCmdBuilder() *AnsiblePlaybookCmdBuilder {
-	return &AnsiblePlaybookCmdBuilder{
-		cmd: &AnsiblePlaybookCmd{},
-	}
-}
+// // NewAnsiblePlaybookCmdBuilder return a new AnsiblePlaybookCmdBuilder
+// func NewAnsiblePlaybookCmdBuilder() *AnsiblePlaybookCmdBuilder {
+// 	return &AnsiblePlaybookCmdBuilder{
+// 		cmd: &AnsiblePlaybookCmd{},
+// 	}
+// }
 
-// Build return a AnsiblePlaybookCmd struct
-func (b *AnsiblePlaybookCmdBuilder) Build() *AnsiblePlaybookCmd {
-	return b.cmd
-}
+// // Build return a AnsiblePlaybookCmd struct
+// func (b *AnsiblePlaybookCmdBuilder) Build() *AnsiblePlaybookCmd {
+// 	return b.cmd
+// }
 
-// WithAnsible return a AnsiblePlaybookCmdBuilder withthe binary file set
-func (b *AnsiblePlaybookCmdBuilder) WithBinary(binary string) *AnsiblePlaybookCmdBuilder {
-	b.cmd.Binary = binary
+// // WithAnsible return a AnsiblePlaybookCmdBuilder withthe binary file set
+// func (b *AnsiblePlaybookCmdBuilder) WithBinary(binary string) *AnsiblePlaybookCmdBuilder {
+// 	b.cmd.Binary = binary
 
-	return b
-}
+// 	return b
+// }
 
-// WithPlaybooks returns an AnsiblePlaybookCmdBuilder with the ansible's playbooks list set
-func (b *AnsiblePlaybookCmdBuilder) WithPlaybooks(playbooks []string) *AnsiblePlaybookCmdBuilder {
-	b.cmd.Playbooks = playbooks
+// // WithPlaybooks returns an AnsiblePlaybookCmdBuilder with the ansible's playbooks list set
+// func (b *AnsiblePlaybookCmdBuilder) WithPlaybooks(playbooks []string) *AnsiblePlaybookCmdBuilder {
+// 	b.cmd.Playbooks = playbooks
 
-	return b
-}
+// 	return b
+// }
 
-// WithOptions returns an AnsiblePlaybookCmdBuilder with the ansible's playbook options set
-func (b *AnsiblePlaybookCmdBuilder) WithOptions(options *AnsiblePlaybookOptions) *AnsiblePlaybookCmdBuilder {
-	b.cmd.Options = options
+// // WithOptions returns an AnsiblePlaybookCmdBuilder with the ansible's playbook options set
+// func (b *AnsiblePlaybookCmdBuilder) WithOptions(options *AnsiblePlaybookOptions) *AnsiblePlaybookCmdBuilder {
+// 	b.cmd.Options = options
 
-	return b
-}
+// 	return b
+// }
 
-// WithConnectionOptions returns an AnsiblePlaybookCmdBuilder with the ansible's playbook specific options for connection set
-func (b *AnsiblePlaybookCmdBuilder) WithConnectionOptions(options *options.AnsibleConnectionOptions) *AnsiblePlaybookCmdBuilder {
-	b.cmd.ConnectionOptions = options
+// // WithConnectionOptions returns an AnsiblePlaybookCmdBuilder with the ansible's playbook specific options for connection set
+// func (b *AnsiblePlaybookCmdBuilder) WithConnectionOptions(options *options.AnsibleConnectionOptions) *AnsiblePlaybookCmdBuilder {
+// 	b.cmd.ConnectionOptions = options
 
-	return b
-}
+// 	return b
+// }
 
-// WithPrivilegeEscalationOptions returns an AnsiblePlaybookCmdBuilder with the ansible's playbook privilege escalation options set
-func (b *AnsiblePlaybookCmdBuilder) WithPrivilegeEscalationOptions(options *options.AnsiblePrivilegeEscalationOptions) *AnsiblePlaybookCmdBuilder {
-	b.cmd.PrivilegeEscalationOptions = options
+// // WithPrivilegeEscalationOptions returns an AnsiblePlaybookCmdBuilder with the ansible's playbook privilege escalation options set
+// func (b *AnsiblePlaybookCmdBuilder) WithPrivilegeEscalationOptions(options *options.AnsiblePrivilegeEscalationOptions) *AnsiblePlaybookCmdBuilder {
+// 	b.cmd.PrivilegeEscalationOptions = options
 
-	return b
-}
+// 	return b
+// }
